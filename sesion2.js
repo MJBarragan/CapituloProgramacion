fbf83e31d94253cc2ab6c1f2716618dc39f6de12
Marijo Barragan <marijox254@gmail.com>
2021-07-17 17:02:54 -0700

    Sesion 2 Capítulo Programación

---
+++ sesion2.js
@@ -0,0 +1,34 @@
+/*
+//declaración de variable
+
+var numero = 365;
+//console.log(numero);
+numero = 4;
+console.log(numero);
+
+//declaracion de constante
+
+const mensaje = "hola";
+//console.log(mensaje);
+//mensaje = "adios";
+console.log(mensaje);
+*/
+
+//características de una persona
+const nombre = "Juana";
+const appelidoPaterno = "Perez";
+const apellidoMaterno = "Ramirez";
+var edad = 17;
+const lugarNacimiento = "Coahuila";
+
+/*console.log(lugarNacimiento);
+console.log(nombre);*/
+
+//define 5 características de un carro
+var color = "blanco";
+const año = 2000;
+const modelo = "jetta";
+var placas = "www";
+const motor = 2;
+
+console.log(color);
